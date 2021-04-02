8633f519d858586acccdf3ac9423300bc81f0922
rketchum95 <79775250+rketchum95@users.noreply.github.com>
2021-04-02 14:36:27 -0700

    Update respository with final code

---
+++ PyBank/main.py
@@ -0,0 +1,80 @@
+import os
+import csv
+
+csv_budgetdata = os.path.join('Resources','budget_data.csv')
+
+#Variable definitions
+months=[]
+monthly_changes=[]
+
+count_months=0
+net_profit=0
+previous_profit=0
+current_profit=0
+MoM_change=0
+total_change=0
+
+with open(csv_budgetdata, newline='',encoding='utf-8') as csvfile:
+	csvreader=csv.reader(csvfile,delimiter=",")
+	
+	csv_header=next(csvreader)
+
+	for row in csvreader:
+		#Count months
+		count_months+=1
+		current_profit=int(row[1])
+		net_profit+=int(row[1])
+
+		if count_months==1:
+			previous_profit=current_profit
+		else:
+			#calculate Month over Month(MoM) change
+			MoM_change=current_profit-previous_profit
+
+			#add each month to month list
+			months.append(row[0])
+
+			#append to monthly changes list
+			monthly_changes.append(MoM_change)
+						
+			#Reset previous month profit for next loop
+			previous_profit = current_profit
+
+	#calculate total changes over time period
+	total_change+=MoM_change
+
+	#calculate average changes over period, minus 1 month for first month
+	average_change=round(total_change/(count_months-1),2)
+
+	#find max increase/decrease in monthly changes
+	max_increase = max(monthly_changes)
+	min_increase = min(monthly_changes)
+
+	#create index for max increase/decrease
+	high_month_index = monthly_changes.index(max_increase)	
+	low_month_index = monthly_changes.index(min_increase)
+
+	#find month of max increase/decreaes
+	best_month=months[high_month_index]
+	worst_month=months[low_month_index]
+
+#print analysis to terminal:	
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {count_months}")
+print(f"Total: ${net_profit}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {best_month}  ${max_increase}")
+print(f"Greatest Decrease in Profits: {worst_month} ${min_increase}")
+
+#export analysis to text file:
+
+filename = 'PyBank_Analysis.txt'
+with open (filename,'w') as file_object:
+	file_object.write("Financial Analysis\n")
+	file_object.write("---------------------\n")
+	file_object.write(f"Total Months: {count_months}\n")
+	file_object.write(f"Total: ${net_profit}\n")
+	file_object.write(f"Average Change: ${average_change}\n")
+	file_object.write(f"Greatest Increase in Profits: {best_month}  ${max_increase}\n")
+	file_object.write(f"Greatest Decrease in Profits: {worst_month} ${min_increase}\n")
