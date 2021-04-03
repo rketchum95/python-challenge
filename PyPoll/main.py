9333e4f21d9ed81c96a18390439ccb94d3b2e168
rketchum95 <79775250+rketchum95@users.noreply.github.com>
2021-04-02 19:56:24 -0700

    Added candidate index & candidate vote count

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,7 @@ import csv
 csv_electiondata = os.path.join('Resources','PyPoll_Resources_election_data.csv')
 
 candidates =[]
-
+candidate_votes=[]
 vote_count=0
 
 
@@ -17,12 +17,25 @@ with open(csv_electiondata, newline='',encoding='utf-8') as csvfile:
 	for row in csvreader:
 		#count #  of votas
 		vote_count+=1
-	
 		
+		#candidate name
+		candidate=row[2]
 
 
+		if candidate in candidates:
+			candidate_index=candidates.index(candidate)
+			candidate_votes[candidate_index]=candidate_votes[candidate_index]+1
+		else:
+			candidates.append(candidate)
+			candidate_votes.append(1)
+
+
+
+
+#print election analysis results to terminal
 print("Election Results")
 print("----------------------------")
 print(f"Total Votes: {vote_count}")
+print(f"{candidates}: ({candidate_votes})")
 
 
