7153cc155c3b7ac59bcd2bbc82a5af5d82ea2088
rketchum95 <79775250+rketchum95@users.noreply.github.com>
2021-04-06 15:33:08 -0700

    Ave sentence length calculation added

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -22,8 +22,8 @@ print(word_count)
 
 #count of sentences
 sentence_count=data.count('.')
-#ave_sentence_length = (len(sentence_count)/(len(data.split()))
-#print(ave_sentence_length)
+ave_sentence_length = word_count/sentence_count
+print(ave_sentence_length)
 
 #Terminal printout:
 print("Paragraph Analysis")
@@ -31,6 +31,6 @@ print("Paragraph Analysis")
 print(f"Approximate Word Count: {word_count}")
 print(f"Approximate Sentence Count: {sentence_count}")
 #print(f"Average Letter Count: {words}")
-#print(f"Average Sentence Length: {ave_sentence_length}")
+print(f"Average Sentence Length: {ave_sentence_length}")
 
 
