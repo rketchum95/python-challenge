0d5f1c86438db43d1f82742b1fce34184a7ef1f6
rketchum95 <79775250+rketchum95@users.noreply.github.com>
2021-04-06 15:29:50 -0700

    initial code for PyParagraph

---
+++ PyParagraph/main.py
@@ -0,0 +1,36 @@
+#PyParagraph:
+#Import a text file filled with a paragraph of your choosing.
+
+#Assess the passage for each of the following:
+	#Approximate word count
+	#Approximate sentence count
+	#Approximate letter count (per word)
+	#Average sentence length (in words)
+
+
+f = open("raw_data\paragraph_1.txt")
+data = f.read()
+print(f.read())
+
+#count of words
+words=data.split()
+
+word_count = len(data.split())
+print(word_count)
+# count of characters
+
+
+#count of sentences
+sentence_count=data.count('.')
+#ave_sentence_length = (len(sentence_count)/(len(data.split()))
+#print(ave_sentence_length)
+
+#Terminal printout:
+print("Paragraph Analysis")
+#print(f"Approximate Word Count: ,len(words)")
+print(f"Approximate Word Count: {word_count}")
+print(f"Approximate Sentence Count: {sentence_count}")
+#print(f"Average Letter Count: {words}")
+#print(f"Average Sentence Length: {ave_sentence_length}")
+
+
