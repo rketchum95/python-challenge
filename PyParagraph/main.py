3bfa03eacc18708785a279fff071840a5a67fc2b
rketchum95 <79775250+rketchum95@users.noreply.github.com>
2021-04-06 16:58:02 -0700

    Complete code

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -12,25 +12,35 @@ f = open("raw_data\paragraph_1.txt")
 data = f.read()
 print(f.read())
 
+words=[]
 #count of words
 words=data.split()
 
 word_count = len(data.split())
-print(word_count)
+
 # count of characters
+character_count = len(data)
+#print(character_count)
+
+special_character_count=data.count(" ")+data.count(".")\
++data.count(",")+data.count("(")+data.count(")")+data.count("-")
+#print(special_character_count)
+
+ave_word_length = round(((character_count-special_character_count)/word_count),1)
+#print(ave_word_length)
 
 
 #count of sentences
 sentence_count=data.count('.')
 ave_sentence_length = word_count/sentence_count
-print(ave_sentence_length)
+#print(ave_sentence_length)
 
 #Terminal printout:
 print("Paragraph Analysis")
-#print(f"Approximate Word Count: ,len(words)")
+print("-------------------------")
 print(f"Approximate Word Count: {word_count}")
 print(f"Approximate Sentence Count: {sentence_count}")
-#print(f"Average Letter Count: {words}")
+print(f"Average Letter Count: {ave_word_length}")
 print(f"Average Sentence Length: {ave_sentence_length}")
 
 
